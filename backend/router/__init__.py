c2798710f0e52e99d4d113c621403d20f28cb37b
choosak kwanraksri <btk1392545@gmail.com>
2024-08-31 23:42:56 +0700

    revert

--- backend/router/__init__.py
+++ backend/router/__init__.py
@@ -16,6 +16,6 @@ router.include_router(item.router, prefix="/items", tags=["Items"])
 router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 router.include_router(user.router, prefix="/users", tags=["Users"])  # include the user router
 router.include_router(exchange.router, prefix="/exchanges", tags=["Exchanges"])
-router.include_router(chat.router, prefix="/chats", tags=["Chatss"])
+router.include_router(chat.router, prefix="/chats", tags=["Chats"])
 def get_router():
     return router
